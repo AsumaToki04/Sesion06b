eaecbfb477c1a6fab574d502a6b1e776f9884c48
AsumaToki04 <megamanzxu@hotmail.com>
2024-12-03 20:25:03 -0500

    Initial Commit

---
+++ Sesion06b/Sesion06bApp.swift
@@ -0,0 +1,20 @@
+//
+//  Sesion06bApp.swift
+//  Sesion06b
+//
+//  Created by DAMII on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesion06bApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
